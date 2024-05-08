6b6077088580aaab5b4e89afbc277c2212716b1a
Robert Jackson <robert.jackson@keyrus.com>
2024-05-08 17:59:25 -0400

    flow

--- scripts/run_nextflow.sh
+++ scripts/run_nextflow.sh
@@ -1,3 +1,2 @@
-STUFF=`pwd`
-echo $STUFF > /tmp/output
-#/usr/local/bin/nextflow  -C nextflow.config run main.nf -profile batch
+cd /opt/nextflow
+/usr/local/bin/nextflow  -C nextflow.config run main.nf -profile batch
